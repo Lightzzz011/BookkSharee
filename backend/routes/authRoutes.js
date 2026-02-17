bc96f43bb3aca24198191d01d8072a27429f7b0e
Lightzzz011 <luckyizdone@gmail.com>
2026-02-17 06:12:11 +0530

    Added authentication routes

---
+++ backend/routes/authRoutes.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+
+const supabase = require("../config/supabase");
+
+const router = express.Router();
+
+// REGISTER
+router.post("/register", async (req, res) => {
+  try {
+    const { name, email, phone, location, password } = req.body;
+
+    if (!name || !email || !phone || !location || !password) {
+      return res.status(400).json({ message: "All fields are required" });
+    }
+
+    // check existing user
+    const { data: existingUser } = await supabase
+      .from("users")
+      .select("*")
+      .eq("email", email)
+      .single();
+
+    if (existingUser) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    const hashedPass = await bcrypt.hash(password, 10);
+
+    const { data, error } = await supabase.from("users").insert([
+      {
+        name,
+        email,
+        phone,
+        location,
+        password: hashedPass,
+      },
+    ]);
+
+    if (error) {
+      return res.status(500).json({ message: error.message });
+    }
+
+    return res.status(201).json({
+      message: "User Registered Successfully",
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const { data: user, error } = await supabase
+      .from("users")
+      .select("*")
+      .eq("email", email)
+      .single();
+
+    if (!user || error) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+      expiresIn: "2d",
+    });
+
+    return res.json({
+      message: "Login Successful",
+      token,
+      user: {
+        id: user.id,
+        name: user.name,
+        email: user.email,
+        phone: user.phone,
+        location: user.location,
+      },
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+});
+
+module.exports = router;
